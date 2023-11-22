b6d083e2def134b1dca882244730c2185830d002
anasfalah3 <anasfalah3@gmail.com>
2023-11-22 22:40:54 +0100

    completing javascript

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const urlField = document.querySelector(".field input");
 previewArea = document.querySelector(".preview-area");
 imgTag = document.querySelector(".thumbnail");
+hiddenInput = document.querySelector(".hidden-input");
 
 urlField.onkeyup = () => {
   let imgUrl = urlField.value; //getting user entered value
@@ -11,9 +12,17 @@ urlField.onkeyup = () => {
     let vidId = imgUrl.split("v=")[1].substring(0, 11); //splitting yt video url from v= so we can get only video
     let ytThumbUrl = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
     imgTag.src = ytThumbUrl;
-  } else if (imgUrl.indexOf("https://www.youtu.be/") != -1) {
+  } else if (imgUrl.indexOf("https://youtu.be/") != -1) {
     // if video url is look like this
+    let vidId = imgUrl.split("be/")[1].substring(0, 11); //splitting yt video url from be/ so we can get only video
+    let ytThumbUrl = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
+    imgTag.src = ytThumbUrl;
   } else if (imgUrl.match(/\.(jpe?g|png|gif|bmp|webp)$/i)) {
     // if entered value is other image file url
+    imgTag.src = imgUrl;
+  }else{
+    imgTag.src = "";
+    previewArea.classList.remove("active")
   }
+  hiddenInput.value = imgTag.src
 };
